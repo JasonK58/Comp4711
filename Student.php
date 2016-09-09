458046da8bd4e1d359509665453c788ded82588e
Jason Kolenosky <JasonK58@gmail.com>
2016-09-09 11:17:07 -0700

    Add and display student information.

--- Student.php
+++ Student.php
@@ -34,4 +34,14 @@ class Student {
         }
         return $total / count($this->grades);
     }
+    
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' ('.$this->average().")\n";
+        foreach($this->emails as $which=>$what){
+            $result .= $which . ': '. $what;
+            $result .= "\n";
+        }
+        return '<pre>'.$result.'</pre>';
+    }
 }
